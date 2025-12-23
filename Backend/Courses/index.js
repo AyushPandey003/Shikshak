ffe656d8ce3c304c2175ec7cf261d986d5be094c
Meet Bikhani <bikhanimeet@gmail.com>
2025-12-23 15:32:51 +0530

    feat: Add Courses service with dedicated model, controllers, and API routes, and update gitignore to exclude package-lock.json.

--- Backend/Courses/index.js
+++ Backend/Courses/index.js
@@ -1,7 +1,10 @@
 import express from "express";
-import authRoutes from "./routes/courses.js";
 import uploadRoutes from "./routes/upload.js";
 import connectDB from "./db/index.js";
+import coursesRoutes from "./routes/courses.js";
+import moduleRoutes from "./routes/modules.js";
+import reviewsRoutes from "./routes/reviews.js";
+
 const app = express();
 
 app.use(express.json());
@@ -9,13 +12,22 @@ app.use(express.json());
 connectDB();
 
 // Health check
-app.get("/", (req, res) => {
+app.get("/api/health", (req, res) => {
+  console.log("[COURSES] Health check");
   res.send("Courses Service running 🚀");
 });
 
 // Courses routes
-app.use("/", authRoutes);
-app.use("/upload", uploadRoutes);
+app.use("/api/courses", coursesRoutes);
+
+// Upload routes
+app.use("/api/upload", uploadRoutes);
+
+// module routes
+app.use("/api/module", moduleRoutes);
+
+// reviews routes
+app.use("/api/reviews", reviewsRoutes);
 
 const PORT = 4002;
 app.listen(PORT, () => {
