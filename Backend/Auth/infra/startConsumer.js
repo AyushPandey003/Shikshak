670cc39c6be846e2eafd2a0e77f3e8e7186059c5
Meet Bikhani <bikhanimeet@gmail.com>
2025-12-25 22:46:25 +0530

    added kafka for course add, update, delete

--- Backend/Auth/infra/startConsumer.js
+++ Backend/Auth/infra/startConsumer.js
@@ -1,5 +1,8 @@
 // infra/startConsumer.js
 import "./email.consumer.js";
 import "./add_course.consumer.js";
+import "./delete_course.consumer.js";
 
 console.log("Email Consumer Service Started!");
+console.log("Add Course Consumer Service Started!");
+console.log("Delete Course Consumer Service Started!");
--- Backend/Courses/infra/startConsumer.js
+++ Backend/Courses/infra/startConsumer.js
@@ -1,4 +1,6 @@
 // infra/startConsumer.js
 import "./material.consumer.js";
+import "./update_redis.consumer.js";
 
 console.log("Material Consumer Service Started!");
+console.log("Update Redis Consumer Service Started!");
