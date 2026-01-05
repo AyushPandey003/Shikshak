f2905c159557777499d82c5bd64f2a0ee998378e
Meet Bikhani <bikhanimeet@gmail.com>
2026-01-05 22:24:26 +0530

    Integrated create test route on viva platform

--- frontend/app/aitest/create/page.tsx
+++ frontend/app/aitest/create/page.tsx
@@ -1,20 +1,54 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import AssessmentForm from '../../../components/AssessmentForm';
 import { AssessmentConfig } from '../../../types/types';
+import axios from 'axios';
+import { useAppStore } from '@/store/useAppStore';
 
 const CreateTestPage: React.FC = () => {
     const router = useRouter();
+    const searchParams = useSearchParams();
+    const [course_id, setCourseId] = useState('');
+    const [user_id, setUserId] = useState('');
+    const { profile } = useAppStore();
 
-    const handleStartSetup = (config: AssessmentConfig) => {
+    useEffect(() => {
+        if (profile?.role !== 'teacher') {
+            router.push('/');
+        }
+    }, [profile, router]);
+
+    const data = searchParams.get('data');
+
+    useEffect(() => {
+        if (data) {
+            try {
+                const parsedData = JSON.parse(data);
+                setCourseId(parsedData.course_id);
+                setUserId(parsedData.user_id);
+            } catch (e) {
+                console.error("Failed to parse data query param", e);
+            }
+        }
+    }, [data]);
+
+
+    const handleStartSetup = async (config: AssessmentConfig) => {
         // Save config to local storage to be accessed by the start page
         if (typeof window !== 'undefined') {
             localStorage.setItem('assessment_config', JSON.stringify(config));
         }
         console.log(config);
         // call api to create a test
+        await axios.post('http://localhost:4000/material/test-create', {
+            title: config.title,
+            questions: config.questions,
+            course_id: course_id,
+            user_id: user_id,
+            valid_until: config.validUntil,
+        }, { withCredentials: true });
         alert('Test created successfully.');
     };
 
